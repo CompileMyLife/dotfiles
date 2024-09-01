ddd34cd04d4dbff8bffe049b0baca2e1c57a3d16
CompileMyLife <ibcinematografy@gmail.com>
2024-09-01 14:57:03 -0700

    setup script for automating environment setup

---
+++ setup.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+#
+# Setup script to automate the creation of dotfiles and setting up git on a new
+# machine
+#
+
+ABS_PATH=$(pwd)
+
+# ----- setup.sh Usage -----
+usage() {
+    echo "Usage: $0 [[-h] [-e someone@domain.com]]"
+    echo "Setup dotfiles in your home directory and setup ssh key for github"
+    echo "Options:"
+    echo -e "\t-h\tHelp\n\t-e\tGitHub email address to assign ssh key"
+}
+
+# ----- Symlink dotfiles to $HOME -----
+create_symlink() {
+    files=".vimrc .vim .tmux.conf"
+
+    for i in $files; do 
+        # Check IF it exists
+        if [ -e "$HOME/$i" ]; then
+            echo -e "The file $i exists in $HOME.\nPlease double check :)"
+        # Check IF vim files
+        elif [[ "$i" == ".vimrc" || "$i" == ".vim" ]]; then
+            echo -e "The file/dir $i doesn't exist in $HOME.\nCreating symlink to $HOME"
+            ln -s $ABS_PATH/vim/$i $HOME # symlink to home
+        # Other .files
+        else
+            echo -e "The file/dir $i doesn't exist in $HOME.\nCreating symlink to $HOME"
+            ln -s $ABS_PATH/$i $HOME # symlink to home
+        fi
+    done
+}
+
+# ----- Setting up github key -----
+create_ssh_key() {
+    ssh-keygen -t ed25519 -C $1 # create ssh-key using ed25519
+    eval "$(ssh-agent -s)"      # startup ssh-agent
+
+    # Check IF config file exists
+    if [ -e $HOME/.ssh/config]; then
+        config_str="  IdentityFile ~/.ssh/id_ed25519"
+        printf "$config_str" >> $HOME/.ssh/config
+    else
+        config_str="Host *\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_ed25519"
+        printf "$config_str" > $HOME/.ssh/config
+    fi
+
+    ssh-add ~/.ssh/id_ed25519
+
+    echo "SSH Key completed successfully. Now add the below public key on GitHub"
+    cat ~/.ssh/id_ed25519.pub
+}
+
+# ----- MAINLINE -----
+
+ghub_email="" # empty
+
+while getopts ":e:h" arg; do
+    case $arg in
+        e) ghub_email=${OPTARG}
+           echo "Email provided: $ghub_email"
+
+           # Check IF email variable empty
+           if [[ -z "$ghub_email" ]]; then
+               echo "Argument has no value"
+               usage
+               exit 1
+           fi
+           ;;
+
+        h) usage
+           exit 0
+           ;;
+
+        \?) 
+           usage
+           echo "Invalid option: -${OPTARG}" >&2
+           exit 1
+           ;;
+
+       :) echo "Option -${OPTARG} requires an argument" >&2
+          usage
+          exit 1
+          ;;
+   esac
+done
+           
+create_symlink
+create_ssh_key $ghub_email
+
+exit 0
